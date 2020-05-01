c5cd7bac4a20c04e0441867dfe4afe76f0bbe487
joseivson <joseivson@gmail.com>
2020-04-30 21:52:22 -0300

    Update 3 30-04-2020

--- analysis_covid19_pernambuco.py
+++ analysis_covid19_pernambuco.py
@@ -11,6 +11,8 @@ df_recife = pd.read_csv('data/covid19-recife.csv')
 df_recife['date'] = pd.to_datetime(df_recife['date'], yearfirst=True, format='%Y-%m-%d')
 df_pe = pd.read_csv('data/covid19-pernambuco.csv')
 df_pe['date'] = pd.to_datetime(df_pe['date'], yearfirst=True, format='%Y-%m-%d')
+df_pe_city = pd.read_csv('data/covid19-pernambuco_per_city.csv')
+df_pe_city['date'] = pd.to_datetime(df_pe_city['date'], yearfirst=True, format='%Y-%m-%d')
 
 compare_places(df_br[df_br['state'] == 'SP'],
                 df_br[df_br['state'] == 'PE'],
@@ -99,8 +101,39 @@ plt.savefig('figs/taxa_mortalidade_estados_brasil.png')
 plt.close()
 
 insert_column(df_br, 'death_per_100k', 'deaths', 'estimated_population_2019')
+df_br['death_per_100k'] = df_br['death_per_100k'].apply(per_100k)
 plt.figure(figsize=(16,9))
-plt.bar(df_br[last]['state'], df_br[last]['death_per_100k'].apply(per_100k))
+plt.bar(df_br[last]['state'], df_br[last]['death_per_100k'])
 plt.title('Mortos por COVID19 a cada 100 mil habitantes nos estados')
 plt.savefig('figs/mortalidade_100k_estados_brasil.png')
+plt.close()
+
+plt.figure(figsize=(16,9))
+plt.scatter(df_br[last]['death_per_100k'], 
+            df_br[last]['confirmed_per_100k_inhabitants'])
+for state in df_br[last]['state']:
+    plt.text(df_br[last & (df_br['state']==state)]['death_per_100k'], 
+            df_br[last & (df_br['state']==state)]['confirmed_per_100k_inhabitants'],
+            state)
+plt.title('Casos e Mortes por COVID19')
+plt.xlabel('Mortes a cada 100 mil habitantes')
+plt.ylabel('Casos a cada 100 mil habitantes')
+plt.savefig('figs/confirmed_x_death_per_100k_estados.png')
+plt.close()
+
+last = df_pe_city['is_last']
+insert_column(df_pe_city, 'death_per_100k', 'deaths', 'estimated_population_2019')
+df_pe_city['death_per_100k'] = df_pe_city['death_per_100k'].apply(per_100k)
+plt.figure(figsize=(16,9))
+plt.scatter(df_pe_city[last]['death_per_100k'], 
+            df_pe_city[last]['confirmed_per_100k_inhabitants'])
+for city in df_pe_city[last]['city']:
+    if (df_pe_city[last & (df_pe_city['city']==city)]['death_per_100k'] > 0).bool():
+        plt.text(df_pe_city[last & (df_pe_city['city']==city)]['death_per_100k'], 
+                df_pe_city[last & (df_pe_city['city']==city)]['confirmed_per_100k_inhabitants'],
+                city)
+plt.title('Casos e Mortes por COVID19')
+plt.xlabel('Mortes a cada 100 mil habitantes')
+plt.ylabel('Casos a cada 100 mil habitantes')
+plt.savefig('figs/confirmed_x_death_per_100k_cidades.png')
 plt.close()
